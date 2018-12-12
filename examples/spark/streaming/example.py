baab5323a8e1f2e1bcee893aaa3036ab2177c513
Nikola Todorovic <nikola.todorovic@realmarket.io>
2018-12-12 23:44:05 +0100

    Added streaming/example.py

--- examples/spark/streaming/example.py
+++ examples/spark/streaming/example.py
@@ -1,15 +1,48 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode
-from pyspark.sql.functions import split
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import explode
+# from pyspark.sql.functions import split
 
-spark = SparkSession \
-    .builder \
-    .appName("StructuredNetworkWordCount") \
-    .getOrCreate()
+# spark = SparkSession \
+#     .builder \
+#     .appName("StructuredNetworkWordCount") \
+#     .getOrCreate()
     
-df = spark \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "kafka:9092") \
-  .option("subscribe", "bitcoin") \
-  .load()
+# df = spark \
+#   .readStream \
+#   .format("kafka") \
+#   .option("kafka.bootstrap.servers", "kafka:9092") \
+#   .option("subscribe", "bitcoin") \
+#   .load()
+
+from __future__ import print_function
+
+import sys
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="PythonStreamingKafkaBitcoin")
+	
+	# Suppress the output logs
+    sc.setLogLevel("ERROR")
+	
+    ssc = StreamingContext(sc, 2)
+
+    directKafkaStream = KafkaUtils.createDirectStream(ssc, ["bitcoin"], {"metadata.broker.list": "kafka:9092"})
+	
+    ssc.checkpoint("checkpoint")
+
+    def updateFunc(new_values, last_sum):
+        return sum(new_values) + (last_sum or 0)
+
+
+    running_counts = directKafkaStream.flatMap(lambda tx: tx.split(" "))\
+                          .map(lambda word: (word, 1))\
+                          .updateStateByKey(updateFunc)
+
+    running_counts.pprint()
+
+    ssc.start()
+    ssc.awaitTermination()
